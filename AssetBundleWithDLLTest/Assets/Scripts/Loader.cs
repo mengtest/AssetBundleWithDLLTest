85d1e0a6492b498e8b9ac6aecac16ac0437d5362
Akira Suzuki <towe1hos@gmail.com>
2016-04-21 02:08:07 +0900

    Divide two download modes

--- AssetBundleWithDLLTest/Assets/Scripts/Loader.cs
+++ AssetBundleWithDLLTest/Assets/Scripts/Loader.cs
@@ -8,20 +8,44 @@ public class Loader : MonoBehaviour {
 	// ex) /Users/Shared/AssetResources/ -> /AssetBundleWithDLLTest/AssetBundleWithDLLTest/Assets/AssetBundles_Output/
 	string BundleURL = "file:///Users/Shared/AssetResources/packingassets.unity3d";
 	string AssetName = "NowPrintingCube";
+	int version = 1;
 
-	IEnumerator Start () {
+	void Start() {
+		StartCoroutine (DownloadAndCache ());
+	}
+
+	IEnumerator DownloadOnly () {
+		Debug.Log ("DownloadOnly mode");
 		using (WWW www = new WWW (BundleURL)) {
 			yield return www;
 			if (www.error != null) {
 				throw new Exception ("WWW download error:" + www.error);
 			}
-			AssetBundle bundle = www.assetBundle;
-			if (AssetName == "") {
-				Instantiate (bundle.mainAsset);
-			} else {
-				Instantiate (bundle.LoadAsset (AssetName));
+			LoadAsset (www.assetBundle, AssetName);
+			www.assetBundle.Unload (false);
+		}
+	}
+
+	IEnumerator DownloadAndCache () {
+		Debug.Log ("DownloadAndCache mode");
+		while (!Caching.ready) {
+			yield return null;
+		}
+		using (WWW www = WWW.LoadFromCacheOrDownload (BundleURL, version)) {
+			yield return www;
+			if (www.error != null) {
+				throw new Exception ("WWW download error:" + www.error + " AssetVersion:" + version);
 			}
-			bundle.Unload (false);
+			LoadAsset (www.assetBundle, AssetName);
+			www.assetBundle.Unload (false);
+		}
+	}
+
+	private void LoadAsset(AssetBundle bundle, string asset_name) {
+		if (asset_name == "") {
+			Instantiate (bundle.mainAsset);
+		} else {
+			Instantiate (bundle.LoadAsset (asset_name));
 		}
 	}
 	
